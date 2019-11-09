89732e1a0a99b87186693b912c65f0b920819274
uopsdod <uopsdod@gmail.com>
2019-11-09 10:00:21 +0800

    refactor

--- CrawlBookPython3001/spiders/CrawlBook.py
+++ CrawlBookPython3001/spiders/CrawlBook.py
@@ -13,8 +13,8 @@ DONE go to the second page (yield response.follow)
 DONE get the category information
 DONE output a rudimentary json output file 
 DONE fix the chinese character issue in the output file 
-* get the original price information and output it 
-* clean the code a bit to comply with pep8
+DONE get the original price information and output it 
+DONE clean the code a bit to comply with pep8
 * sort and aggregate the category information 
 * structure the data in the output file (some layers required)
 * clean the code a bit to comply with pep8 
@@ -31,51 +31,31 @@ class QuotesSpider(scrapy.Spider):
     '''requests are scheduled and processed asynchronously.'''
     name = 'books'
     start_urls = [
-        'https://www.books.com.tw/web/sys_tdrntb/books/',
-        # 'http://quotes.toscrape.com/tag/humor/',
+        'https://www.books.com.tw/web/sys_tdrntb/books/'
     ]
 
     def parse(self, response):
-        # 書名/作者/原價
-        # yield {
-        #     'result': response.xpath('//title')
-        #     # 'result': response
-        # }
-        books = list();
 
         bookcount = 0;
-
         for general_selector in response.xpath('//body//div[@class="mod type02_m035 clearfix"]//li[contains(@class,"item")]//div[@class="type02_bd-a"]'): # use extract to get the textual data
-            bookname_selectorlist = Selector(text=general_selector.get()).xpath('//a[contains(@href,"loc")]//text()') # this is how you reuse previous bigger Selector
+            bookname_selectorlist = Selector(text=general_selector.get()).xpath('//a[contains(@href,"loc")]//text()') # this is how you reuse previous bigger Selector, probably because Selector needs <html><body>...</html></body> to wrap the content to work
             bookauthor_selectorlist = Selector(text=general_selector.get()).xpath('//a[contains(@href,"author")]//text()') # this is how you reuse previous bigger Selector
             book_discount_price_selectorlist = Selector(text=general_selector.get()).xpath('//ul[@class="msg"]//li[@class="price_a"]//strong[2]//b//text()') # this is how you reuse previous bigger Selector
 
-
-            # 書名
-            # print("bookname - {:s}".format(bookname_selectorlist.get()))
-            print("count - {:d}".format(bookcount))
+            # get bookname, bookauthor, discount_price on the 1st-layer page
             book = Book();
             book.bookname = bookname_selectorlist[0].extract();
             book.author = bookauthor_selectorlist[0].extract();
-            # if len(book_discount_price_selectorlist) != 0:
             book.discount_price = book_discount_price_selectorlist[0].extract();  # TODO: restore it
 
-            # get the inner page link of a book
+            # go to the 2nd-layer page to get category and original_price
             inner_url = Selector(text=general_selector.get()).xpath('//a/@href')[0].extract();
-            print("hey004")
-            # print(type(inner_url))
-            print(book.bookname)
-            print(inner_url)
             inner_url_request = response.follow(inner_url, callback=self.parse_inner_url) # must add yield
             inner_url_request.meta['data'] = book; # meta is a special field in the Request class to let you pass data down the river
             yield inner_url_request;
 
-            books.append(book);
-            # books[counter] = book;
+            # increment bookcount to track the total number of books we crawl
             bookcount += 1;
-
-
-
 
         # verify the final book list
         print("bookcount - {:d}".format(bookcount))
@@ -88,20 +68,19 @@ class QuotesSpider(scrapy.Spider):
     def parse_inner_url(self, response):
         book = response.meta['data']; # get the data passed from the previous request
 
-        # 分類
+        # category
         category = ''
         for breadcrumb_selector in response.xpath('//body//ul[@class="container_24 type04_breadcrumb"]//li[contains(@itemtype,"Breadcrumb")]//a//span//text()'):
             category += breadcrumb_selector.extract() + ">"
         category = category[:-1] # remove the last character
         book.category = category
 
-
-        # 原價 TODO:NEXT
+        # original_price
         original_price = response.xpath('//body//div//div//div//div//div[@class="cnt_prod002 clearfix"]//div//div//ul[@class="price"]//li//em//text()')[0].extract()
         book.original_price = original_price
         # printObject("hey009",original_price)
 
-        # output - 書名/作者/分類/原價/折扣價
+        # output 
         yield {
             'book_name': book.bookname,
             'book_author': book.author,
