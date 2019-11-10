af6396374baf1a88a9aeed826572c0ce43ddecf9
uopsdod <uopsdod@gmail.com>
2019-11-10 21:42:34 +0800

    turn discountworker into a class

--- CrawlBookPython3001/workers/discountworker.py
+++ CrawlBookPython3001/workers/discountworker.py
@@ -4,40 +4,42 @@ import json
 import io
 import math
 
-def execute_discount():
-    # get n% book according to the discount percentage
-    with io.open('output/bookcrawler_result.jl', 'r', encoding='utf-8-sig') as f, io.open('output/discountworker_result.txt', 'w', encoding='utf-8-sig') as fw:
-        dict = {};  # put it up here to record all data
-        count = 0;
-        books = list()
-        for line in f:
-            line = line.encode('utf-8-sig')[3:].decode('utf-8-sig')  # this works too
-            bookJsonObj = json.loads(line)
+class Discountworker:
+    """A class to process discount-related information"""
+    def execute_discount(self):
+        # get n% book according to the discount percentage
+        with io.open('output/bookcrawler_result.jl', 'r', encoding='utf-8-sig') as f, io.open('output/discountworker_result.txt', 'w', encoding='utf-8-sig') as fw:
+            dict = {};  # put it up here to record all data
+            count = 0;
+            books = list()
+            for line in f:
+                line = line.encode('utf-8-sig')[3:].decode('utf-8-sig')  # this works too
+                bookJsonObj = json.loads(line)
 
-            if 'LOGIN_REQUIRED' == bookJsonObj['original_price']:
-                continue
+                if 'LOGIN_REQUIRED' == bookJsonObj['original_price']:
+                    continue
 
-            bookJsonObj['discount_percentage'] = (float(bookJsonObj['original_price']) - float(bookJsonObj['discount_price'])) / float(bookJsonObj['original_price'])
-            books.append(bookJsonObj)
+                bookJsonObj['discount_percentage'] = (float(bookJsonObj['original_price']) - float(bookJsonObj['discount_price'])) / float(bookJsonObj['original_price'])
+                books.append(bookJsonObj)
 
-        # sort the books according to the discount percentage in a descending order
-        books = sorted(books, key=lambda i: i['discount_percentage'], reverse=True)
-        # filter the n% books out
-        books_filtered = listTopNthBooks(books, relative_nth=50) # relative_nth range from 0 ~ 100
-        print("booksFiltered count: from {:d} -> {:d}".format(len(books), len(books_filtered)))
-        # show result
-        print(books_filtered)
-        # store the output in dictionary format
-        for book in books_filtered:
-            fw.write(str(book))
-            fw.write("\r\n")
+            # sort the books according to the discount percentage in a descending order
+            books = sorted(books, key=lambda i: i['discount_percentage'], reverse=True)
+            # filter the n% books out
+            books_filtered = self.listTopNthBooks(books, relative_nth=50) # relative_nth range from 0 ~ 100
+            print("booksFiltered count: from {:d} -> {:d}".format(len(books), len(books_filtered)))
+            # show result
+            print(books_filtered)
+            # store the output in dictionary format
+            for book in books_filtered:
+                fw.write(str(book))
+                fw.write("\r\n")
 
-def listTopNthBooks(list, relative_nth):
-    if relative_nth < 0:
-        print("negative value for relative_nth is not allowed")
-        return list[0:0];
-    nth = math.floor(len(list) * (relative_nth / 100)) # floor the nth number to get top n percentage elementnt
-    return list[0:nth]
+    def listTopNthBooks(self, list, relative_nth):
+        if relative_nth < 0:
+            print("negative value for relative_nth is not allowed")
+            return list[0:0];
+        nth = math.floor(len(list) * (relative_nth / 100)) # floor the nth number to get top n percentage elementnt
+        return list[0:nth]
 
 def printObject(identifier, obj):
     print(identifier)
@@ -45,4 +47,4 @@ def printObject(identifier, obj):
     print(obj)
 
 if __name__ == "__main__":
-    execute_discount()
+    Discountworker().execute_discount()
