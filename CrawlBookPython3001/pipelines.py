b7b538044685f5baa0e51f5ba3e25e353334cbe0
uopsdod <uopsdod@gmail.com>
2019-11-11 00:16:21 +0800

    comply pipelines.py with pep8

--- CrawlBookPython3001/pipelines.py
+++ CrawlBookPython3001/pipelines.py
@@ -8,6 +8,7 @@ from CrawlBookPython3001.workers.categoryworker import Categoryworker
 from CrawlBookPython3001.workers.discountworker import Discountworker
 import json
 
+
 class Crawlbookpython3001Pipeline(object):
 
     def __init__(self, bookcrawler_output_file_path):
@@ -16,7 +17,9 @@ class Crawlbookpython3001Pipeline(object):
     @classmethod
     def from_crawler(cls, crawler):
         return cls(
-            bookcrawler_output_file_path = crawler.settings.get('BOOKCRAWLER_OUTPUT_FILE_PATH')
+            bookcrawler_output_file_path=crawler.settings.get(
+                'BOOKCRAWLER_OUTPUT_FILE_PATH'
+            )
         )
 
     def open_spider(self, spider):
@@ -25,16 +28,16 @@ class Crawlbookpython3001Pipeline(object):
 
     def process_item(self, item, spider):
         print('Crawlbookpython3001Pipeline process_item() called')
-        line = json.dumps(dict(item),ensure_ascii=False) + "\n"
+        line = json.dumps(dict(item), ensure_ascii=False) + "\n"
         self.file.write(line)
         return item
 
     def close_spider(self, spider):
         print('Crawlbookpython3001Pipeline close_spider() called')
         self.file.close()
-        Categoryworker().execute_category(source_file_path = self.bookcrawler_output_file_path)
-        Discountworker().execute_discount(source_file_path = self.bookcrawler_output_file_path)
-
-
-
-
+        Categoryworker().execute_category(
+            source_file_path=self.bookcrawler_output_file_path
+        )
+        Discountworker().execute_discount(
+            source_file_path=self.bookcrawler_output_file_path
+        )
