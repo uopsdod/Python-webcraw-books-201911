2775a554b8b929b6990bac85788ca0bf04118112
uopsdod <uopsdod@gmail.com>
2019-11-10 23:57:29 +0800

    comply Discountworker.py with pep8

--- CrawlBookPython3001/workers/discountworker.py
+++ CrawlBookPython3001/workers/discountworker.py
@@ -4,29 +4,36 @@ import json
 import io
 import math
 
+
 class Discountworker:
     """A class to process discount-related information"""
     def execute_discount(self, source_file_path):
         # get n% book according to the discount percentage
-        with io.open(source_file_path, 'r', encoding='utf-8-sig') as f, io.open('CrawlBookPython3001/output/discountworker_result.txt', 'w', encoding='utf-8-sig') as fw:
-            dict = {};  # put it up here to record all data
-            count = 0;
+        output_path = 'CrawlBookPython3001/output/discountworker_result.txt'
+        encoding_str = 'utf-8-sig'
+        with io.open(source_file_path, 'r', encoding=encoding_str) as f, \
+                io.open(output_path, 'w', encoding=encoding_str) as fw:
+            dict = {}  # put it up here to record all data
+            count = 0
             books = list()
             for line in f:
-                line = line.encode('utf-8-sig')[3:].decode('utf-8-sig')  # this works too
+                line = line.encode(encoding_str)[3:].decode(encoding_str)
                 bookJsonObj = json.loads(line)
 
                 if 'LOGIN_REQUIRED' == bookJsonObj['original_price']:
                     continue
 
-                bookJsonObj['discount_percentage'] = (float(bookJsonObj['original_price']) - float(bookJsonObj['discount_price'])) / float(bookJsonObj['original_price'])
+                oprice = float(bookJsonObj['original_price'])
+                dprice = float(bookJsonObj['discount_price'])
+                bookJsonObj['discount_percentage'] = (oprice - dprice) / oprice
                 books.append(bookJsonObj)
 
-            # sort the books according to the discount percentage in a descending order
-            books = sorted(books, key=lambda i: i['discount_percentage'], reverse=True)
+            # sort the books according to the discount in a descending order
+            sortk = 'discount_percentage'
+            books = sorted(books, key=lambda i: i[sortk], reverse=True)
             # filter the n% books out
-            books_filtered = self.listTopNthBooks(books, relative_nth=48) # relative_nth range from 0 ~ 100
-            print("booksFiltered count: from {:d} -> {:d}".format(len(books), len(books_filtered)))
+            # relative_nth range from 0 ~ 100
+            books_filtered = self.listTopNthBooks(books, relative_nth=48)
             # show result
             print(books_filtered)
             # store the output in dictionary format
@@ -37,11 +44,13 @@ class Discountworker:
     def listTopNthBooks(self, list, relative_nth):
         if relative_nth < 0:
             print("negative value for relative_nth is not allowed")
-            return list[0:0];
-        nth = math.floor(len(list) * (relative_nth / 100)) # floor the nth number to get top n percentage elementnt
+            return list[0:0]
+        # floor the nth number to get top n percentage elementnt
+        nth = math.floor(len(list) * (relative_nth / 100))
         return list[0:nth]
 
-def printObject(identifier, obj):
+
+def print_object(identifier, obj):
     print(identifier)
     print(type(obj))
     print(obj)
