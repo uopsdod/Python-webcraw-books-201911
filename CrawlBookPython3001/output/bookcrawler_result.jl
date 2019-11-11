5ad115ac806e72c51897b865f73ae60f6ceb37af
uopsdod <uopsdod@gmail.com>
2019-11-11 19:56:23 +0800

    add output files

---
+++ CrawlBookPython3001/output/bookcrawler_result.jl
@@ -0,0 +1,100 @@
+{"book_name": "有你的旅程：黃氏兄弟寫真紀實（特裝版）", "book_author": "黃氏兄弟", "category": "博客來>中文書>生活風格>影視偶像>明星書>華人明星", "original_price": "LOGIN_REQUIRED", "discount_price": "680"}
+{"book_name": "鄭家純2020年《小朋友才選擇，橫衝直撞我都要》寫真桌曆 組合", "book_author": "鄭家純", "category": "博客來>中文書>生活風格>性愛指南/寫真集>性感寫真", "original_price": "LOGIN_REQUIRED", "discount_price": "880"}
+{"book_name": "孿生子【限量作者親簽版】", "book_author": "藤崎彩織", "category": "博客來>中文書>文學小說>翻譯文學>日本文學", "original_price": "360", "discount_price": "284"}
+{"book_name": "108課綱科學素養小達人：看故事培養解決問題的超能力(孩子的第一套STEAM繪遊書精選套書)", "book_author": "胡儀芬,劉益廷,潘美慧", "category": "博客來>中文書>童書/青少年文學>圖畫書>知識繪本", "original_price": "1200", "discount_price": "792"}
+{"book_name": "有你的旅程：黃氏兄弟寫真紀實", "book_author": "黃氏兄弟", "category": "博客來>中文書>生活風格>影視偶像>明星書>華人明星", "original_price": "480", "discount_price": "379"}
+{"book_name": "故事學：學校沒教，你也要會的表達力", "book_author": "歐陽立中", "category": "博客來>中文書>童書/青少年文學>語言學習>中文>閱讀/作文", "original_price": "280", "discount_price": "221"}
+{"book_name": "山元式頭針除痛療法：馬上有感的神奇頭針療法", "book_author": "山元 敏勝", "category": "博客來>中文書>醫療保健>中醫>中醫療法>經絡/針灸", "original_price": "300", "discount_price": "237"}
+{"book_name": "蔡康永的情商課2：因為這是你的人生", "book_author": "蔡康永", "category": "博客來>中文書>心理勵志>情緒/壓力", "original_price": "350", "discount_price": "277"}
+{"book_name": "原子習慣：細微改變帶來巨大成就的實證法則", "book_author": "詹姆斯‧克利爾", "category": "博客來>中文書>商業理財>成功法>自我成長", "original_price": "330", "discount_price": "261"}
+{"book_name": "是愛不是礙，是伴不是絆：圓滿自在的親密關係 哈佛醫師心能量", "book_author": "許瑞云", "category": "博客來>中文書>心理勵志>兩性與家庭關係>兩性關係", "original_price": "380", "discount_price": "300"}
+{"book_name": "顧爸媽，這樣做最安心：15項迷思╳18種常見老年病╳25則日常伴老須知，台大老年醫學權威詹鼎正親自解惑", "book_author": "詹鼎正,李翠卿", "category": "博客來>中文書>醫療保健>家庭醫藥保健", "original_price": "380", "discount_price": "300"}
+{"book_name": "深夜食堂 22", "book_author": "安倍夜郎", "category": "博客來>中文書>漫畫>其他類型>溫馨療癒", "original_price": "240", "discount_price": "190"}
+{"book_name": "屁屁偵探 噗噗！ 雪山的白色怪物？！", "book_author": "Troll", "category": "博客來>中文書>童書/青少年文學>圖畫書>幻想/冒險", "original_price": "280", "discount_price": "221"}
+{"book_name": "鯨吞億萬：一個大馬年輕人，行騙華爾街與好萊塢的真實故事", "book_author": "湯姆‧萊特,布萊利‧霍普", "category": "博客來>中文書>商業理財>傳記>人物傳記", "original_price": "450", "discount_price": "356"}
+{"book_name": "財務自由的人生：跟著首席分析師楊應超學華爾街的投資技巧和工作效率，40歲就過FIRE的優質生活", "book_author": "楊應超", "category": "博客來>中文書>商業理財>投資理財>理財規劃>個人理財", "original_price": "350", "discount_price": "277"}
+{"book_name": "解憂電影院（獨家燙金夜光書衣版）：那些電影教我的事2019最新力作！", "book_author": "水ㄤ,水某", "category": "博客來>中文書>心理勵志>勵志故事/散文>勵志小品文", "original_price": "360", "discount_price": "284"}
+{"book_name": "總有一天你會懂：從0到100歲，該學會的人生大事，都在這些生活的小事裡了", "book_author": "海可．法樂", "category": "博客來>中文書>生活風格>圖文書/繪本>翻譯圖文書/繪本", "original_price": "499", "discount_price": "394"}
+{"book_name": "屁屁偵探讀本：好景莊有妖怪", "book_author": "Troll", "category": "博客來>中文書>童書/青少年文學>兒童故事/小說>偵探/冒險", "original_price": "280", "discount_price": "221"}
+{"book_name": "你的善良必須有點鋒芒2：如何聰明善良，才能讓你做個內心柔軟，但有骨氣的好人？", "book_author": "慕顏歌", "category": "博客來>中文書>心理勵志>人際關係>人脈/處世", "original_price": "300", "discount_price": "237"}
+{"book_name": "大會計師教你從財報數字看懂經營本質", "book_author": "張明輝", "category": "博客來>中文書>商業理財>會計/統計>財務報表", "original_price": "380", "discount_price": "300"}
+{"book_name": "解憂起笑店：八耐舜子的塗鴉日記(隨書附贈POP字體練習塗鴉本+起笑貼)", "book_author": "八耐舜子", "category": "博客來>中文書>生活風格>圖文書/繪本>華文圖文書/繪本", "original_price": "350", "discount_price": "277"}
+{"book_name": "與焦慮和解：克服過度完美主義、拖延症、害怕批評，從自我檢測中找回生活平衡的實用指南", "book_author": "愛麗絲．博耶斯", "category": "博客來>中文書>心理勵志>情緒/壓力", "original_price": "320", "discount_price": "253"}
+{"book_name": "刑警家的孩子", "book_author": "宮部美幸", "category": "博客來>中文書>文學小說>懸疑/推理小說>日本懸疑/推理小說", "original_price": "380", "discount_price": "300"}
+{"book_name": "6分鐘日記的魔法：最簡單的書寫，改變你的一生【1書+1日記本】", "book_author": "多明尼克．斯賓斯特", "category": "博客來>中文書>商業理財>成功法>自我成長", "original_price": "360", "discount_price": "284"}
+{"book_name": "沉默的遊行", "book_author": "東野圭吾", "category": "博客來>中文書>文學小說>懸疑/推理小說>日本懸疑/推理小說", "original_price": "420", "discount_price": "332"}
+{"book_name": "夢想路上，遇見防彈與36位哲學家：無論你在生命的哪個階段，不管你認不認識BTS，都可以在本書找到築夢的勇氣、愛自己的力量！★隨書贈送BTS七位成員的個人哲學小卡（隨機2款）", "book_author": "車玟周", "category": "博客來>中文書>心理勵志>生活哲學", "original_price": "320", "discount_price": "253"}
+{"book_name": "禮悟：在脆弱的盡頭，看見生命出口", "book_author": "蔣承縉,李小光", "category": "博客來>中文書>心理勵志>勵志故事/散文>真實人生故事", "original_price": "380", "discount_price": "300"}
+{"book_name": "打火哥的30堂烈焰求生課：第一線熱血消防員親授關鍵保命絕招，破解火場迷思", "book_author": "蔡宗翰", "category": "博客來>中文書>自然科普>應用科學>其他", "original_price": "350", "discount_price": "277"}
+{"book_name": "原則：生活和工作", "book_author": "瑞．達利歐", "category": "博客來>中文書>商業理財>成功法>自我成長", "original_price": "600", "discount_price": "474"}
+{"book_name": "刻意練習：原創者全面解析，比天賦更關鍵的學習法", "book_author": "安德斯‧艾瑞克森,羅伯特‧普爾", "category": "博客來>中文書>心理勵志>人格天賦/潛能開發", "original_price": "320", "discount_price": "253"}
+{"book_name": "末等魂師(7)魂師之路，開啟！", "book_author": "銀千羽", "category": "博客來>中文書>輕小說>華文作品>幻奇冒險", "original_price": "220", "discount_price": "174"}
+{"book_name": "天氣之子", "book_author": "新海誠", "category": "博客來>中文書>輕小說>日本作品>戀愛故事", "original_price": "300", "discount_price": "237"}
+{"book_name": "尋油記：一位品油師對生命的堅持與實踐", "book_author": "王甯", "category": "博客來>中文書>醫療保健>飲食保健>其他", "original_price": "420", "discount_price": "332"}
+{"book_name": "一人公司：為什麼小而美是未來企業發展的趨勢", "book_author": "保羅．賈維斯", "category": "博客來>中文書>商業理財>管理與領導>經營策略", "original_price": "380", "discount_price": "300"}
+{"book_name": "不要在該奮鬥時選擇安逸", "book_author": "老楊的貓頭鷹", "category": "博客來>中文書>心理勵志>勵志故事/散文>勵志小品文", "original_price": "260", "discount_price": "205"}
+{"book_name": "氣炸鍋好好玩料理125：熱炒超美味！蒸煮、油炸、煎烤、烘焙全提案，從新手到進階，網路詢問度最高的油切人氣食譜", "book_author": "徐湘珠,蕭秀珊,施宜孝", "category": "博客來>中文書>飲食>食譜>小家電料理", "original_price": "380", "discount_price": "300"}
+{"book_name": "學會用情：當老莊遇見黃帝內經2", "book_author": "蔡璧名", "category": "博客來>中文書>醫療保健>中醫>中醫養生", "original_price": "499", "discount_price": "394"}
+{"book_name": "窮查理的普通常識（增修版）：巴菲特50年智慧合夥人查理．蒙格的人生哲學", "book_author": "查理‧蒙格", "category": "博客來>中文書>商業理財>傳記>人物傳記", "original_price": "450", "discount_price": "356"}
+{"book_name": "小資女艾蜜莉：我的資產翻倍存股筆記", "book_author": "艾蜜莉（張紫凌）", "category": "博客來>中文書>商業理財>投資理財>股票／證券", "original_price": "280", "discount_price": "221"}
+{"book_name": "超級績效3：動能大師圓桌論壇", "book_author": "馬克．米奈爾維尼,大衛．瑞安,丹．贊格,馬克．里奇二世", "category": "博客來>中文書>商業理財>投資理財>投資學", "original_price": "420", "discount_price": "332"}
+{"book_name": "被討厭的勇氣：自我啟發之父「阿德勒」的教導", "book_author": "岸見一郎,古賀史健", "category": "博客來>中文書>心理勵志>心理學>經典學派/大師思想", "original_price": "300", "discount_price": "237"}
+{"book_name": "產科醫師的好孕教室：讓媽媽安心，寶寶健康的懷孕計畫書", "book_author": "陳勝咸", "category": "博客來>中文書>親子教養>懷孕/小兒>產前/產後", "original_price": "380", "discount_price": "300"}
+{"book_name": "離開時，以我喜歡的樣子（獨家賽璐珞書衣加贈箴言紙膠帶）：日本個性派俳優，是枝裕和電影靈魂演員，樹木希林120則人生語錄", "book_author": "樹木希林", "category": "博客來>中文書>心理勵志>生活哲學", "original_price": "380", "discount_price": "300"}
+{"book_name": "ETF大贏家：股魚教你紅綠燈超簡單投資術", "book_author": "股魚", "category": "博客來>中文書>商業理財>投資理財>債券／基金", "original_price": "330", "discount_price": "261"}
+{"book_name": "出門買蛋去：小川糸的一年份幸福日記", "book_author": "小川糸", "category": "博客來>中文書>文學小說>翻譯文學>日本文學", "original_price": "330", "discount_price": "261"}
+{"book_name": "燃起主管魂：盛治仁的管理私房筆記", "book_author": "盛治仁", "category": "博客來>中文書>商業理財>管理與領導>領導／帶人", "original_price": "350", "discount_price": "277"}
+{"book_name": "零基礎的佛系理財術：只要一招，安心穩穩賺", "book_author": "施昇輝", "category": "博客來>中文書>商業理財>投資理財>理財規劃>個人理財", "original_price": "350", "discount_price": "277"}
+{"book_name": "日日減醣瘦身料理：肉品海鮮．蔬食沙拉．鍋物料理，吃飽吃滿還瘦18公斤，無痛減醣瘦身家常菜111道", "book_author": "張晴琳（圈媽）", "category": "博客來>中文書>飲食>食譜>其他/綜合", "original_price": "380", "discount_price": "300"}
+{"book_name": "金庸作品集平裝版(1～36冊)", "book_author": "金庸", "category": "博客來>中文書>文學小說>歷史/武俠小說>金庸武俠小說", "original_price": "9000", "discount_price": "6299"}
+{"book_name": "82年生的金智英", "book_author": "趙南柱", "category": "博客來>中文書>文學小說>翻譯文學>亞洲文學", "original_price": "310", "discount_price": "245"}
+{"book_name": "投資最重要的事：一本股神巴菲特讀了兩遍的書", "book_author": "霍華．馬克斯", "category": "博客來>中文書>商業理財>投資理財>投資學", "original_price": "380", "discount_price": "300"}
+{"book_name": "窮人的經濟學：如何終結貧窮？", "book_author": "阿比吉特‧班納吉,艾絲特‧杜芙若", "category": "博客來>中文書>社會科學>經濟學", "original_price": "450", "discount_price": "356"}
+{"book_name": "無節操☆Bitch社 3", "book_author": "おげれつたなか", "category": "LOGIN_REQUIRED", "original_price": "LOGIN_REQUIRED", "discount_price": "126"}
+{"book_name": "七侯筆錄之筆靈（上）（下）套書（首刷限量PVC書衣版）（拆封不退）", "book_author": "馬伯庸", "category": "博客來>中文書>文學小說>歷史/武俠小說>其他武俠小說", "original_price": "900", "discount_price": "711"}
+{"book_name": "不！我不喜歡被捉弄", "book_author": "梅麗莎．希金斯", "category": "博客來>中文書>童書/青少年文學>圖畫書>生活教育", "original_price": "280", "discount_price": "210"}
+{"book_name": "薩提爾的對話練習：以好奇的姿態，理解你的內在冰山，探索自己，連結他人（書+有聲CD兩片）", "book_author": "李崇建", "category": "博客來>中文書>親子教養>生活教養", "original_price": "400", "discount_price": "316"}
+{"book_name": "OKR：做最重要的事", "book_author": "約翰‧杜爾", "category": "博客來>中文書>商業理財>管理與領導>管理學", "original_price": "450", "discount_price": "356"}
+{"book_name": "富爸爸，窮爸爸（20週年紀念版）", "book_author": "羅勃特‧T‧清崎", "category": "博客來>中文書>商業理財>投資理財>理財規劃>個人理財", "original_price": "380", "discount_price": "300"}
+{"book_name": "愛上氣炸鍋100天：椒麻雞．蜜汁叉燒．紙包魚．戚風蛋糕，天天開炸也不膩的100道美味氣炸鍋料理", "book_author": "人愛柴", "category": "博客來>中文書>飲食>食譜>小家電料理", "original_price": "360", "discount_price": "284"}
+{"book_name": "全新制20次多益滿分的怪物講師TOEIC多益單字+文法（附1CD+防水書套）", "book_author": "怪物講師教學團隊（台灣）", "category": "博客來>中文書>語言學習>語言能力檢定測驗>NEWTOEIC新多益>字彙", "original_price": "499", "discount_price": "394"}
+{"book_name": "大器可以晚成：當世界沉迷年少得志，耐心是你成功的本事", "book_author": "里奇．卡爾加德", "category": "博客來>中文書>商業理財>成功法>自我成長", "original_price": "380", "discount_price": "300"}
+{"book_name": "謝沅瑾鼠年生肖運勢大解析：史上最萬用的開運工具書，謝老師親算二〇二〇農民曆、流年流月，一書在案，平安加持、財旺運開！隨書附贈謝沅瑾老師親自開光加持「五帝錢福祿五行掛飾」", "book_author": "謝沅瑾", "category": "博客來>中文書>宗教命理>命相/命理>曆書", "original_price": "399", "discount_price": "315"}
+{"book_name": "解壓放空店：八耐舜子的笑看青春集（隨書附贈放空日記塗鴉本+解壓句子貼）", "book_author": "八耐舜子", "category": "博客來>中文書>生活風格>圖文書/繪本>華文圖文書/繪本", "original_price": "380", "discount_price": "300"}
+{"book_name": "厭世廢文觀止：英雄豪傑競靠腰，國文課本沒有教", "book_author": "厭世國文老師", "category": "博客來>中文書>文學小說>文學研究>華文文學研究", "original_price": "320", "discount_price": "253"}
+{"book_name": "歡迎來到實力至上主義的教室 (9)（特裝版）", "book_author": "衣笠彰梧", "category": "博客來>中文書>輕小說>日本作品>幻奇冒險", "original_price": "LOGIN_REQUIRED", "discount_price": "550"}
+{"book_name": "走過愛的蠻荒：撕掉羞恥印記，與溫柔同行的偏鄉教師", "book_author": "文國士", "category": "博客來>中文書>心理勵志>勵志故事/散文>真實人生故事", "original_price": "320", "discount_price": "253"}
+{"book_name": "真確：扭轉十大直覺偏誤，發現事情比你想的美好", "book_author": "漢斯．羅斯林,奧拉．羅斯林,安娜．羅朗德", "category": "博客來>中文書>商業理財>經濟／趨勢>觀念／趨勢", "original_price": "400", "discount_price": "316"}
+{"book_name": "ONE PIECE航海王 94", "book_author": "尾田榮一郎", "category": "博客來>中文書>漫畫>動作冒險>冒險／探險", "original_price": "105", "discount_price": "95"}
+{"book_name": "故事課1：3分鐘說18萬個故事，打造影響力", "book_author": "許榮哲", "category": "博客來>中文書>商業理財>職場工作術>溝通說話", "original_price": "350", "discount_price": "277"}
+{"book_name": "覺醒的你：暢銷百萬，歐普拉的床頭靈修書", "book_author": "麥克‧辛格", "category": "博客來>中文書>宗教命理>新時代>靈性療癒/成長", "original_price": "280", "discount_price": "221"}
+{"book_name": "台灣獨曆2020：台灣歷史上的今天", "book_author": "Mock Mayson", "category": "博客來>中文書>人文史地>台灣史地>通史", "original_price": "700", "discount_price": "553"}
+{"book_name": "股市就是你的印鈔機：市井股神的籌碼飆股獲利法", "book_author": "林上仁,郭勝", "category": "博客來>中文書>商業理財>投資理財>股票／證券", "original_price": "360", "discount_price": "284"}
+{"book_name": "習慣致富：成為有錢人，你不需要富爸爸，只需要富習慣", "book_author": "湯姆．柯利,麥可．雅德尼", "category": "博客來>中文書>商業理財>成功法>致富", "original_price": "380", "discount_price": "300"}
+{"book_name": "你好，我是接體員", "book_author": "大師兄", "category": "博客來>中文書>心理勵志>生死醫病", "original_price": "320", "discount_price": "253"}
+{"book_name": "創客創業導師程天縱的經營學：翻轉企業經營與創業困境的32個創見", "book_author": "程天縱", "category": "博客來>中文書>商業理財>管理與領導>經營策略", "original_price": "380", "discount_price": "342"}
+{"book_name": "改革中國報業的無冕王：黃天鵬傳記", "book_author": "黃佩珊", "category": "博客來>中文書>人文史地>中國史地>人物史/傳記", "original_price": "500", "discount_price": "395"}
+{"book_name": "鄭家純2020年『直入你心』寫真桌曆", "book_author": "鄭家純", "category": "博客來>中文書>生活風格>性愛指南/寫真集>性感寫真", "original_price": "LOGIN_REQUIRED", "discount_price": "480"}
+{"book_name": "給不小心就會太在意的你：停止腦中小劇場，輕鬆卸下內心的重擔！", "book_author": "水島廣子", "category": "博客來>中文書>心理勵志>個人成長>心靈成長", "original_price": "300", "discount_price": "237"}
+{"book_name": "一日三餐減醣料理：單週無壓力消失2kg的美味計劃，72道低醣速瘦搭配餐", "book_author": "娜塔", "category": "博客來>中文書>飲食>食譜>其他/綜合", "original_price": "380", "discount_price": "300"}
+{"book_name": "叛逆南極：失敗者的遺言(親筆簽名版)", "book_author": "宥勝", "category": "博客來>中文書>心理勵志>勵志故事/散文>真實人生故事", "original_price": "450", "discount_price": "356"}
+{"book_name": "新制多益NEW TOEIC聽力／閱讀題庫解析【博客來獨家套書】（附4 MP3）", "book_author": "Hackers Academia", "category": "博客來>中文書>語言學習>語言能力檢定測驗>NEWTOEIC新多益>攻略/題庫", "original_price": "1760", "discount_price": "1162"}
+{"book_name": "你，是自己的鑰匙：靈媒媽媽的心靈解答書", "book_author": "Ruowen Huang", "category": "博客來>中文書>心理勵志>人格天賦/潛能開發", "original_price": "350", "discount_price": "277"}
+{"book_name": "存股輕鬆學：4年存300張金融股，每年賺自己的13%（超值加贈存股SOP投資影音）", "book_author": "孫悟天", "category": "博客來>中文書>商業理財>投資理財>股票／證券", "original_price": "380", "discount_price": "300"}
+{"book_name": "有錢人想的和你不一樣", "book_author": "T. Harv Eker", "category": "博客來>中文書>商業理財>經濟／趨勢>金融", "original_price": "250", "discount_price": "198"}
+{"book_name": "減醣快瘦 氣炸鍋料理：減醣實證者小魚媽╳體態雕塑營養師Ricky，量身打造58道增肌減脂減重餐", "book_author": "小魚媽,Ricky營養師", "category": "博客來>中文書>飲食>食譜>小家電料理", "original_price": "380", "discount_price": "300"}
+{"book_name": "新制多益 NEW TOEIC 單字大全：2018起多益更新單字資訊完全掌握！(附13小時8種版本MP3)", "book_author": "David Cho", "category": "博客來>中文書>語言學習>語言能力檢定測驗>NEWTOEIC新多益>字彙", "original_price": "499", "discount_price": "394"}
+{"book_name": "科學實驗王47：傳染病與病原體", "book_author": "Story a.", "category": "博客來>中文書>童書/青少年文學>知識學習漫畫>科普", "original_price": "300", "discount_price": "237"}
+{"book_name": "零極限：創造健康、平靜與財富的夏威夷療法", "book_author": "喬．維泰利、伊賀列卡拉．修．藍博士", "category": "博客來>中文書>宗教命理>新時代>靈性療癒/成長", "original_price": "280", "discount_price": "221"}
+{"book_name": "我用1檔ETF存自己的18%", "book_author": "陳重銘", "category": "博客來>中文書>商業理財>投資理財>股票／證券", "original_price": "360", "discount_price": "284"}
+{"book_name": "莫斯科紳士", "book_author": "亞莫爾．托歐斯", "category": "博客來>中文書>文學小說>翻譯文學>美國文學", "original_price": "450", "discount_price": "356"}
+{"book_name": "蝴蝶朵朵 （附贈應用指導手冊，兩冊不分售）", "book_author": "幸佳慧", "category": "博客來>中文書>童書/青少年文學>圖畫書>生活教育", "original_price": "350", "discount_price": "277"}
+{"book_name": "蔡康永的情商課：為你自己活一次", "book_author": "蔡康永", "category": "博客來>中文書>心理勵志>情緒/壓力", "original_price": "350", "discount_price": "277"}
+{"book_name": "GIVEN 被贈與的未來(05)", "book_author": "キヅナツキ", "category": "博客來>中文書>漫畫>BL / GL>BL", "original_price": "150", "discount_price": "128"}
+{"book_name": "雖然想死，但還是想吃辣炒年糕：身心俱疲，卻渾然不覺，一位「輕鬱症」女孩與精神科醫師的12週療癒對話，陪你擁抱不完美的自己", "book_author": "白洗嬉", "category": "博客來>中文書>心理勵志>心理諮商/治療", "original_price": "340", "discount_price": "269"}
+{"book_name": "蒙特梭利教養進行式：翩翩園長的45個正向教養解方", "book_author": "何翩翩", "category": "博客來>中文書>親子教養>教育現場>幼兒教育", "original_price": "380", "discount_price": "300"}
+{"book_name": "比句點更悲傷", "book_author": "大師兄", "category": "博客來>中文書>心理勵志>生死醫病", "original_price": "320", "discount_price": "253"}
+{"book_name": "代謝力UP減醣好好：體重輕鬆瘦，體脂降10％的升級版技巧", "book_author": "娜塔", "category": "博客來>中文書>生活風格>塑身美妝>瘦身美體", "original_price": "360", "discount_price": "284"}
+{"book_name": "Jay的跑步筆記：矽谷工程師激勵上萬人的科學化訓練與生活哲學", "book_author": "許立杰", "category": "博客來>中文書>生活風格>運動/戶外活動>跑步", "original_price": "420", "discount_price": "332"}
+{"book_name": "為什麼你的退休金只有別人的一半？", "book_author": "闕又上", "category": "博客來>中文書>商業理財>投資理財>理財規劃>退休/家計規劃", "original_price": "480", "discount_price": "379"}
+{"book_name": "鄭家純2020年『愛是永橫』寫真桌曆", "book_author": "鄭家純", "category": "博客來>中文書>生活風格>性愛指南/寫真集>性感寫真", "original_price": "LOGIN_REQUIRED", "discount_price": "480"}
